f31775555e9a8171171941291533d8ce3ba658f8
Carson Rajcan <carajcan@gmail.com>
2016-12-18 04:34:12 -0500

    Make a basic User model (including secure passwords)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+ 
+  attr_accessor :name, :email
   def new
+    
+    def initialize(attributes = {})
+    @name  = attributes[:name]
+    @email = attributes[:email]
+    @password = attributes[:password]
+    end
   end
+
 end
