b2fc5eaf39d582bc3f1938b12225b66049ece4b1
Carson Rajcan <carajcan@gmail.com>
2017-01-03 17:26:23 -0500

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,29 @@
 class UsersController < ApplicationController
  
-  attr_accessor :name, :email
+  #attr_accessor :name, :email
+
+  def show 
+    @user = User.find(params[:id]) #params gets info from URL
+  end
+
   def new
-    
-    def initialize(attributes = {})
-    @name  = attributes[:name]
-    @email = attributes[:email]
-    @password = attributes[:password]
+    @user = User.new
+  end
+
+  def create 
+    @user = User.new(user_params) #replace this with new user made by html form
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to user_url(@user)
+    else
+      render 'new'
     end
   end
+
+  private 
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 
 end
