b907b4ac7869768b6bd5c150946f52bbf96428f6
crajcan <carajcan@gmail.com>
2017-02-01 22:29:53 -0500

    Update carrier_wave.rb
    remove actual keys

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,9 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAIW4DVF6QTNJDEGOQ'],
-      :aws_secret_access_key => ENV['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']
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  ENV['crajcansampleapp']
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
