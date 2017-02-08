6ba0ebd0c93fda749e76b50f120faa4e1ff06857
Carson Rajcan <carajcan@gmail.com>
2017-02-07 19:11:21 -0500

    another weird fix

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,11 +5,8 @@ if Rails.env.production?
       :provider              => 'AWS',
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-<<<<<<< HEAD
+
       :region                => 'us-west-2'
-=======
-      :region                => 'us-west-1'
->>>>>>> 26c0c32472b5677b698ad2b01ad75f629cefb94a
     }
     config.fog_directory     =  ENV['S3_BUCKET']
   end
